25fbde04911f153ea89488051174826477a7ab57
NeoLeijona <danteg18@hotmail.com>
2014-02-28 14:46:10 +0200

    Gitignore once again
    Toimii ehkä nyt jatkossakin... (tuskin)

---
+++ EVOMotor/Android/jni/Android.mk
@@ -0,0 +1,54 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := evoplatform
+LOCAL_CFLAGS := -D GLM_FORCE_RADIANS
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Platform/Include \
+					$(LOCAL_PATH)/../../depencies
+
+PLATFORM_PATH := ../../Platform/Source
+
+LOCAL_SRC_FILES := \
+	$(PLATFORM_PATH)/Graphics.cpp \
+	$(PLATFORM_PATH)/IndexData.cpp \
+	$(PLATFORM_PATH)/Object.cpp \
+	$(PLATFORM_PATH)/Shader.cpp \
+	$(PLATFORM_PATH)/SpriteBatch.cpp \
+	$(PLATFORM_PATH)/SpriteObject.cpp \
+	$(PLATFORM_PATH)/Texture.cpp \
+	$(PLATFORM_PATH)/VertexData.cpp \
+	$(PLATFORM_PATH)/Viewport.cpp \
+	$(PLATFORM_PATH)/Android/FileReader.cpp 
+	
+
+include $(BUILD_STATIC_LIBRARY)
+
+ 
+
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := evoandroid
+LOCAL_CFLAGS := -D GLM_FORCE_RADIANS
+GAME_PATH := $(LOCAL_PATH)/../../EVOMotor
+PLATFORM_PATH := ../../Platform/Source
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Platform/Include \
+					$(LOCAL_PATH)/../../depencies \
+					$(GAME_PATH)/ 
+
+
+LOCAL_SRC_FILES := \
+					../../EVOMotor/Source/Engine.cpp\
+					$(PLATFORM_PATH)/Android/android_native_app_glue.c \
+					$(PLATFORM_PATH)/Android/Android.cpp
+	 
+
+LOCAL_LDLIBS := -landroid -lEGL -lGLESv2 -llog 
+LOCAL_STATIC_LIBRARIES := evoplatform
+
+include $(BUILD_SHARED_LIBRARY)
+ 
+ 
